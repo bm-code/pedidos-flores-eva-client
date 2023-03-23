1c044661e0b77bd92899070830283efe6d9b38c4
bm-code <i.benimorales@gmail.com>
2023-03-23 10:37:05 +0100

    login correcto

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,63 +1,58 @@
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
+import logo from '../assets/logo.svg'
 
 export default function Login({ login, setLogin }) {
   const initialState = {
     username: '',
     password: ''
   }
-  // const [login, setLogin] = useState({
-  //   name: null,
-  //   username: null,
-  //   email: null
-  // });
-  const [user, setUser] = useState(initialState);
-  const [form, setForm] = useState(initialState);
+  const [datos, setDatos] = useState(initialState)
+  const [loading, setLoading] = useState(false)
 
-  function handleInput(event) {
-    setForm({ ...form, [event.target.name]: event.target.value });
+  const handleInputChange = (e) => {
+    let { name, value } = e.target;
+    let newDatos = { ...datos, [name]: value }
+    setDatos(newDatos);
   }
 
-  function submit(event) {
-    setUser(form);
-    event.preventDefault();
-
-    // Realizamos el GET a la base de datos
-    axios.get('https://pedidos-server.up.railway.app/api/login', {
-      params: {
-        username: user.username,
-        password: user.password
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (datos.username === '' || datos.password === '') {
+      console.log('No enviar');
+    } else {
+      let res = await axios.post('https://pedidos-server.up.railway.app/api/login', datos);
+      if (!datos) {
+        setLoading(true)
+      } else {
+        setLoading(false);
+        setLogin({
+          name: res.data.name,
+          username: res.data.username,
+          email: res.data.email
+        })
+        window.sessionStorage.setItem('name', JSON.stringify(login.name))
+        window.sessionStorage.setItem('username', JSON.stringify(login.username))
+        window.sessionStorage.setItem('email', JSON.stringify(login.email))
       }
     }
-    ).then(response => {
-      if (response.data !== '' && response.data.name !== null) {
-        setLogin({
-          name: response.data.name,
-          username: response.data.username,
-          email: response.data.email
-        })
-        sessionStorage.setItem('name', login.name)
-        sessionStorage.setItem('username', login.username)
-        sessionStorage.setItem('email', login.email)
-      } else {
-        console.log('error en el login');
-      }
-    })
-      .catch(err => {
-        console.log("Error: ", err);
-      });
   }
 
   return (
     <>
       <div className="login-page p-3 d-flex justify-content-center align-items-center flex-column h-100">
+        <img className='mb-3' src={logo} alt="Logo de Flores Eva" />
         <h2 className="mb-3">Iniciar sesión</h2>
-        <form onSubmit={submit}>
-          <input name='username' onChange={handleInput} type="text" className="form-control mb-3" id="username" placeholder="Tu nombre de usuario" value={form.username} />
-          <input name='password' onChange={handleInput} type="password" className="form-control mb-3" id="password" placeholder="Tu contraseña" value={form.password} />
+        <form onSubmit={handleSubmit}>
+          <input required name='username' onChange={handleInputChange} type="text" className="form-control mb-3" id="username" placeholder="Tu nombre de usuario" value={datos.username} />
+          <input required name='password' onChange={handleInputChange} type="password" className="form-control mb-3" id="password" placeholder="Tu contraseña" value={datos.password} />
           <button type='submit' className="btn btn-primary">Iniciar sesión</button>
         </form>
+        {loading &&
+          <div className="spinner-grow mt-5" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>}
       </div>
     </>
   )
